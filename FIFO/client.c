6442f9891b873071f2de86ec5cd41d15d30e8b9b
51ccyxw <18829507975@163.com>
2018-04-29 17:27:15 +0800

    A FIFO EXAMPLE

---
+++ FIFO/client.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<errno.h>
+#include<fcntl.h>
+#include<stdlib.h>
+
+#define FIFO_READ "writefile"
+#define FIFO_WRITE "readfile"
+#define BUF_SIZE 1024
+
+int main()
+{
+	int wfd,rfd;
+	char buf[BUF_SIZE];
+	int len;
+
+	umask(0);
+	if(mkfifo(FIFO_WRITE,S_IFIFO|0666))
+	{
+		printf("Can't create FIFO %s because %s\n",FIFO_WRITE,strerror(errno));
+		exit(1);
+	}
+
+	while((rfd = open(FIFO_READ,O_RDONLY)) == -1)
+	{
+		sleep(1);
+	}
+
+	wfd = open(FIFO_WRITE,O_WRONLY);
+	if(wfd == -1)
+	{
+		printf("Fail to open FIFO %s:%s\n",FIFO_WRITE,strerror(errno));
+		exit(-1);
+	}
+
+	while(1)
+	{
+		len = read(rfd,buf,BUF_SIZE);
+		if(len > 0)
+		{
+			buf[len] = '\0';
+			printf("Server: %s\n",buf);
+		}
+
+		printf("Client: ");
+		fgets(buf,BUF_SIZE,stdin);
+		buf[strlen(buf)-1] = '\0';
+		if(strncmp(buf,"quit",4) == 0)
+		{
+			close(wfd);
+			unlink(FIFO_WRITE);
+			close(rfd);
+			exit(0);
+		}
+		write(wfd,buf,strlen(buf));
+	}
+	return 0;
+}
